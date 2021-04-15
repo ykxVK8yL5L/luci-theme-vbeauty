9f28060603e38c37eabf2e14881b58528d29d88a
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2021-04-15 12:52:51 +0800

    Update vbeauty.lua

--- luci-theme-vbeauty/luasrc/controller/admin/vbeauty.lua
+++ luci-theme-vbeauty/luasrc/controller/admin/vbeauty.lua
@@ -1,15 +1,37 @@
 module("luci.controller.admin.vbeauty", package.seeall)
 
 function index()
+  if not nixio.fs.access("/etc/config/vbeauty") then return end
+  if nixio.fs.access("/etc/config/vbeauty_show") then
     entry({"admin", "status"}, alias("admin", "status", "vbeauty"), _("Status"), 0).index = true
     entry({"admin", "status", "vbeauty"}, template("themes/vbeauty/vbeauty"), _("Vbeauty"), 0)
+  else
+    entry({"admin", "status"}, alias("admin", "status", "overview"), _("Status"), 20).index = true
+    entry({"admin", "status", "vbeauty"}, template("themes/vbeauty/vbeauty"), _("Vbeauty"), 60)
+  end
+    entry({"admin", "status", "vzant", "show"}, call("show_menu")).leaf = true
+    entry({"admin", "status", "vzant", "hide"}, call("hide_menu")).leaf = true
     entry({'admin', 'status', 'vbeauty', 'addfavourite'}, call('addfavourite'))
     entry({'admin', 'status', 'vbeauty', 'delfavourite'}, call('delfavourite'))
     entry({'admin', 'status', 'vbeauty', 'getfavourites'}, call('getfavourites'))
 end
 
+
+
+function show_menu()
+  luci.sys.call("touch /etc/config/vbeauty_show")
+  luci.http.redirect(luci.dispatcher.build_url("admin", "status", "vbeauty"))
+end
+
+function hide_menu()
+  luci.sys.call("rm -rf /etc/config/vbeauty_show")
+  luci.http.redirect(luci.dispatcher.build_url("admin", "status", "overview"))
+end
+
+
+
 function addfavourite()
-  local title = string.gsub(luci.http.formvalue("title"), "%s+", "")
+  local title = luci.http.formvalue("title")
   local src = luci.http.formvalue("src")
   local short = luci.http.formvalue("short")
   local bcolor = luci.http.formvalue("bcolor")
